0254e213ee90c85b56a8590507d27251e1bc9371
feliciengolfier <feloux@gmail.com>
2018-06-17 00:14:30 +0200

    minor chages on interface

--- Assets/Scripts/DisplayClients.cs
+++ Assets/Scripts/DisplayClients.cs
@@ -4,10 +4,22 @@ using UnityEngine;
 
 public class DisplayClients : MonoBehaviour {
 
-    
+    public clientToDisplay[] clientsToDisplay;
 	// Use this for initialization
 	void Start () {
 		
 	}
 	
+    public void DisplayClient(client givenClient)
+    {
+
+    }
+
+    [System.Serializable]
+    public class clientToDisplay
+    {
+        client _client;
+        GameObject display;
+    }
+
 }
